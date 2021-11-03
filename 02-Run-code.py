c67e577dad800600cc5af3d4aa811e8a7d2ce8c6
VerbitskiyAV <Anton.Verbitskiy@moex.com>
2021-11-03 12:40:15 +0300

    Добавим файл с кодом на Pyton

---
+++ 02-Run-code.py
@@ -0,0 +1,56 @@
+"""
+## Запуск кода
+
+- из консоли
+- Из редакторов
+- из IDE
+
+C Python поставляется простой редактор [**IDLE**.](https://docs.python.org/3/library/idle.html)
+
+Дзен питона
+"""
+import this
+
+
+"""
+### Функции помощи
+
+Встроенная помощь
+функция [help](https://docs.python.org/3/library/functions.html#help)
+"""
+help(help)
+
+"""
+В Python все есть объекты
+
+### Список методов класса
+
+функция [dir](https://docs.python.org/3/library/functions.html#dir)
+"""
+dir(int)
+dir(1)
+
+i = 1025
+i.bit_length()
+
+"""
+### Узнать тип объекта
+
+функция [type](https://docs.python.org/3/library/functions.html#type)
+"""
+type(1)
+type('hello')
+type(type)
+
+# Простой пример программы
+# Рассчет чисел Фиббоначи
+n = 10
+a = 0
+b = 1
+f = [a]
+for i in range(n):
+    temp = a
+    a = b
+    b += temp
+    f.append(a)
+print("Fibonacci number for {} is {}".format(n, a))
