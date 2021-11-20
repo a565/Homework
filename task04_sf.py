0d5edaafbdd6441be0faffa702f2f8363c3aba83
VerbitskiyAV <Anton.Verbitskiy@moex.com>
2021-11-20 17:02:58 +0300

    Запись в файл

--- task04_sf.py
+++ task04_sf.py
@@ -14,3 +14,8 @@ soup = BeautifulSoup(response.text, 'html.parser')
 f = open("epam-html.txt", "w", encoding="cp1251")   # "w" - создать файл для записи
 print(soup.text, file=f)           # печать в файл
 f.close()
+
+fb = open("testw.txt", "wb")
+fb.write(b'hello')
+fb.write("Привет".encode("cp1251"))
+fb.close()
