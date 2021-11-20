f8a654ebe40da7fb19b76ff1b567ebed6fea6000
VerbitskiyAV <Anton.Verbitskiy@moex.com>
2021-11-20 17:32:18 +0300

    Запись html в файл

--- task04_sf.py
+++ task04_sf.py
@@ -5,17 +5,12 @@ import requests
 
 #GET запрос без параметров
 
-response = requests.get('https://www.epam.com')
+#response = requests.get('https://www.epam.com')
+response = requests.get('http://localhost:5000/swagger/index.html')
 
 
-from bs4 import BeautifulSoup
-soup = BeautifulSoup(response.text, 'html.parser')
 
-f = open("epam-html.txt", "w", encoding="cp1251")   # "w" - создать файл для записи
-print(soup.text, file=f)           # печать в файл
+f = open("local-html.txt", "wb")
+f.write(response.text.encode("utf-8"))
 f.close()
 
-fb = open("testw.txt", "wb")
-fb.write(b'hello')
-fb.write("Привет".encode("cp1251"))
-fb.close()
