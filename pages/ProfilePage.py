91bec2fd1f686b103939dd503a461bd72a977ca3
averb565 <averb565@yandex.ru>
2021-12-06 19:03:23 +0300

    profile page

---
+++ pages/ProfilePage.py
@@ -0,0 +1,10 @@
+from selenium.webdriver.common.by import By
+
+from pages.base_page import BasePage
+
+
+
+class  ProfilePage(BasePage):
+    _url =('https://esia-portal1.test.gosuslugi.ru/profile/user/personal')
+
+    
