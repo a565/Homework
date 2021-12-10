17e72a1d7bed3a416c048bf1957356395a1f1cbf
averb565 <averb565@yandex.ru>
2021-12-10 14:58:33 +0300

    prof_page

--- pages/ProfilePage.py
+++ pages/ProfilePage.py
@@ -1,10 +1,11 @@
 from selenium.webdriver.common.by import By
-
 from pages.base_page import BasePage
-
-
 
 class  ProfilePage(BasePage):
     _url =('https://esia-portal1.test.gosuslugi.ru/profile/user/personal')
+    text = 'a1@mail.ru'
+
+    def find_text(self, text, _time_out=5):
+        return ProfilePage.text_is_displayed(self,text, _time_out, driver=self._driver)
 
     
